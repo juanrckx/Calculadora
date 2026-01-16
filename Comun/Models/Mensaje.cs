a257a8ff6a9f4ab1e57e52b80faa14c2c2bcc57e
Juan José Rodríguez <jjuam2006@gmail.com>
2026-01-15 21:41:46 -0600

    Creación de la clase Mensaje.cs para la creación de mensajes en la interfaz

---
+++ Comun/Models/Mensaje.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Comun.Models
+{
+    [Serializable]
+    public class Mensaje
+    {
+        public TipoMensaje Tipo { get; set; }
+        public string Contenido { get; set; }
+        public string IdCliente { get; set; }
+        public DateTime Fecha { get; set }
+    }
+}
+
+namespace Comun.Enums
+{
+    public enum TipoMensaje
+    {
+        ConexionEstablecida,
+        ExpresionParaEvaluar,
+        ResultadoCalculado,
+        Error,
+        HistorialSolicitado,
+        HistorialEnviado
+    }
+}
